2315cb6850f41d2be8393505cfd4506f156389fc
Jacob Hammerle <hammerja@miamioh.edu>
2018-03-05 20:09:11 -0500

    employeeEdit form populated with proper data

--- src/components/EmployeeForm.js
+++ src/components/EmployeeForm.js
@@ -59,4 +59,4 @@ const mapStateToProps = (state) => {
 	return { name, phone, shift };
 }
 
-export default connect(null, { employeeUpdate })(EmployeeForm);
+export default connect(mapStateToProps, { employeeUpdate })(EmployeeForm);
